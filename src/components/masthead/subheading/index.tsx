90b5ac8397501913eb2fe7b12d7705e79f67b5c7
William Daghouz <w.daghouz@gmail.com>
2022-01-26 20:21:42 +0100

    Modified fonts and fixed a typo.

--- src/components/masthead/subheading/index.tsx
+++ src/components/masthead/subheading/index.tsx
@@ -5,7 +5,7 @@ import * as subheadingStyles from './subheading.module.css'
 const Heading = () => (
   <p className={subheadingStyles.subheading}>
     Deep-dive into the statistics of CHSB and understand the mechanics of the
-    SwissBorg ecosystem.
+    full SwissBorg ecosystem.
   </p>
 )
 
